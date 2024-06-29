0f3a5db000e13d3df4bffd048954989f12d7dcd3
javeria-farooq <gettingeducation6@gmail.com>
2024-06-29 17:51:00 +0500

    Maintain & delete extra code

--- app.js
+++ app.js
@@ -1,154 +1,70 @@
-// function signUp() {
-//     var sEmail = document.getElementById('sEmail').value
-//     var sPass = document.getElementById('sPass').value
-//     localStorage.setItem('Email', sEmail)
-//     localStorage.setItem('Password', sPass)
-//     location.href = './login.html'
-// }
+/*access signup page */
 
-// function logIn() {
-//     var lEmail = document.getElementById('lEmail').value
-//     var lPass = document.getElementById('lPass').value
-//     if (localStorage.getItem('Email') === lEmail) {
-//         if (localStorage.getItem('Password') === lPass) {
-//             alert('Hurrah! User found')
-//             location.href = './welcome.html'
-//         }
-//     }
+let signEmail = document.querySelector(".signEmail")
+let signPass = document.querySelector(".signPass")
+let signBtn = document.querySelector(".signBtn")
 
-//     else {
-//         alert('Ooops! User Not found')
-//         location.href = './signup.html'
-//     }
+/*access login page */
 
-// }
+let loginEmail = document.querySelector(".loginEmail")
+let loginPass = document.querySelector(".loginPass")
+let loginBtn = document.querySelector(".loginBtn")
 
+let userName = document.querySelector(".userName")
 
+let email = []
+let password = []
+let userFound = false
+let signupContainer = document.querySelector(".signupContainer")
+let loginContainer = document.querySelector(".loginContainer")
+let welcomeContainer = document.querySelector(".welcomeContainer")
 
-// let myObj = {
-//     firstEmail: 'javeria123@gmail.com',
-//     firstPassword: 'jfaya',
-//     secondEmail: 'javeria123@gmail.com',
-//     secondPassword: 'jfaya',
-//     thirdEmail: 'javeria123@gmail.com',
-//     thirdPassword: 'jfaya',
-    // localStorage.setItem(key, myObj[key])
-// }
+/*sign up btn fnc */
 
-// ye bt yd rh k mn loop k bgher obj ko kbh bh print nh krwaskti na hi doc pe na or khin hn bs console pe krwaskti hn
-// lkn console t wse bh rough work h user usko access t nh krskta. inshort mnn obj ko kbhi bh khin bh without the forIn
-// loop k print nh krwaskti or isi trhn mn obj ko local ya session storage mn bh loop k bgher store nh krwaskti.
-
-// for (var key in myObj) {
-//     document.write(key, ' : ', myObj[key], '<br>', '<br>')
-    //    localStorage.setItem(key, myObj[key])
-// }
-
-// localStorage.setItem(key, myObj[key])
-// localStorage.setItem('name', 'javeria')
-
-
-
-
-// function signup() {
-//     var email = document.getElementById("sEmail").value 
-//     var pass = document.getElementById("sPass").value
-//     localStorage.setItem("Email", email)
-//     localStorage.setItem("Password", pass)
-//     location.href = "./login.html"
-// }
-
-// function login() {
-//     var email = document.getElementById("lEmail").value
-//     var pass = document.getElementById("lPass").value
-//     if(localStorage.getItem("Email") === email) {
-//         if (localStorage.getItem("Password") === pass) {
-//             location.href = "./welcome.html";
-//         }
-//     }
-
-//     else{
-//         alert("User Not found")
-//         location.href = "./signup.html"
-//     }
-// }
-
-// function logout() {
-//     localStorage.clear()
-// }
-
-// perfect work strt:
-
-// let myObj = {
-//     Email1: 'javeria@',
-//     Email2: 'aliza@', 
-//     Email3: 'yasmeen@',
-//     Email4: 'afnan@',
-// }
-
-// let sto = JSON.stringify(myObj)
-
-// console.log(sto)
-// for (let key in myObj) {
-//     localStorage.setItem('Email', sto)
-//     localStorage.setItem('Email', sto)
+signBtn.addEventListener("click", () => {
+    email.push(signEmail.value)
+    password.push(signPass.value)
     
-// }
+    let emailString = JSON.stringify(email)
+    let passwordString = JSON.stringify(password)
+    
+    localStorage.setItem("Email", emailString)
+    localStorage.setItem("Password", passwordString)
+    
+    loginContainer.classList.remove("hide")
+    loginContainer.classList.add("active")
+    
+})
 
-// let getting = localStorage.getItem('Email')
-// document.write(getting, "<br>")
-// let get2 = JSON.parse(getting)
-// document.write(get2)
+/*login btn fnc */
 
-// perfect work end.
+loginBtn.addEventListener("click", () => {
+    let getEmail = localStorage.getItem("Email")
+    let getPassword = localStorage.getItem("Password")
 
-// localStorage.setItem('Email', myObj.Email1)
+    let emailObj = JSON.parse(getEmail)
+    let passwordObj = JSON.parse(getPassword)
 
+    for(let key in emailObj) {
+        if(loginEmail.value === emailObj[key]) {
+            for(let key in passwordObj) {
+                if(loginPass.value === passwordObj[key]) {
+                    userFound = true
+                    alert(`Congratulations ${loginEmail.value}! Your account is successfully created.`)
+                    welcomeContainer.classList.remove("hide")
+                    welcomeContainer.classList.add("active")
+                    userName.innerHTML += `<h2> ${loginEmail.value} <h2>`
+                    break
+                }
 
+                else{
+                    userFound = false
+                }
+            }
+        }
+    }
 
-// for (let i = 0; i < 8; i = i+2) {
-//     document.write(get2[i])
-// }
-// let getting = JSON.parse(localStorage.getItem('Email'))
-
-// for (let abc in getting) {
-//     document.write(getting)
-// }
-
-// for (let i = 0; i < getting.length; i++) {
-//     document.write(getting)
-// }
-
-// let getting = localStorage.getItem('Email')
-// document.write(JSON.parse(getting))
-
-// for (var key in getting) 
-
-// let getting = JSON.parse(localStorage.getItem('Email'))
-// document.write(getting)
-
-// document.write(JSON.parse(localStorage.getItem('Email')))
-
-// document.write(JSON.parse(getting))
-
-// document.write(localStorage.getItem('Email'))
-
-// for (var key in myObj) {
-//     localStorage.setItem('Email', myObj[key])
-//     document.write('Email', myObj[key])
-// }
-
-
-
-// let firstName = "javeria"
-// let age = 20
-
-// let lSto = localStorage.setItem('First Name: ', firstName)
-// let lSto2 = localStorage.setItem('Age', age)
-
-// document.write(typeof lSto)
-// document.write(typeof firstName)
-
-// let lGet = localStorage.getItem('Age')
-// document.write(lGet)
-// document.write(typeof lGet)
+    if(userFound === false) {
+        alert("Oops! Your Email doesnot exist.")
+    }
+})
